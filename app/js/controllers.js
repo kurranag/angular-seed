c3c4c3245de7a105f0e7e0dcc801c8e78a495918
unknown <krao@al-KNRao-l01.commandalkon.local>
2014-04-11 08:29:18 -0500

    Week5 assignment

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,37 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
+  controller('MyCtrl1', ['$scope', function ($scope) {
+      $scope.car = { make: 'Honda',
+          model: 'Civic',
+          year: [2010, 2011, 2112, 2013, 2014],
+          price: 24000
+      }
+  } ])
+  .controller('MyCtrl2', ['$scope', function ($scope) {
+      $scope.car = [{ make: 'Honda',
+          model: 'Civic',
+          year: 2010,
+          price: 19000
+      },
+      { make: 'Honda',
+          model: 'Civic',
+          year: 2011,
+          price: 20000
+      },
+      { make: 'Honda',
+          model: 'Civic',
+          year: 2012,
+          price: 21000
+      },
+      { make: 'Honda',
+          model: 'Civic',
+          year: 2013,
+          price: 23000
+      },
+      { make: 'Honda',
+          model: 'Civic',
+          year: 2014,
+          price: 24000
+      }]
+  } ]);
